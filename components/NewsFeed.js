adf677886c94f3fac1b260202ccb5df8b2fd68fc
Toughee <Jimernez.io@gmail.com>
2021-03-20 10:39:10 -0500

    ClearStar/008/Dependencies_Update_003 - Updated dependencies commited audit report fixes inside app

--- components/NewsFeed.js
+++ components/NewsFeed.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-
-
-
 //
-// class Newsfeed extend React.Component
-//
-// render props
+// class Newsfeed extend React.Component {
+// componentDidMount()  {
+//		const apiUrl = '';
+//		fetch(apiUrl)
+// 			.then((response) => response.json())
+//			.then((data) => )
+// render() {
+//	return <>
+//}
 
 // .json
 // axios
 //  'Space.com'
-//
+//}
 //<section>
 
 // {title}<href>{url}</href>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import './App.css';
 
 function NewsFeed() {
@@ -9,7 +8,11 @@ function NewsFeed() {
         <h1>ClearStar</h1>
       </header>
         <section>
-        
+          <ul>
+               <li>
+                 News title : <span className="news-url">working url</span> 
+               </li>
+          </ul>
         </section>
 
     </div>
@@ -17,87 +20,3 @@ function NewsFeed() {
 }
 
 export default NewsFeed;
-
-
-
-//
-//
-//
-// class ClearStar extends React component {
-//
-//}
-//
-//componentDidMount 
-//axios
-
-
-// render() {
-//   return(
-//     const {} = this.state; 
-//     <div>
-//       <section>
-//         <ul>
-//           <li></li>
-//         </ul>
-//       </section>
-//     </div>
-//     )
-// }
-//
-//
-//
-//
-//
-//
-//
-
-
-
-//             <ul>
-//               <li>
-//                 News title w/ working url 
-//               </li>
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-
-//               <li>
-//                 News title w/ working url 
-//               </li>
-//                <li>
-//                 News title w/ working url 
-//               </li>
-//                <li>
-//                 News title w/ working url 
-//               </li>
-
-//              </ul>
-// //
